d912d43736fd44fdf5a4d88bbb08bf1157bd918d
Thomas Barrial <thomas.barrial@gmail.com>
2023-03-13 14:04:39 +0100

    Starter - config

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">Welcome to the home page</h1>
+        </div>
+    )
+}
---
+++ sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import { schemaTypes } from './schemas'
+import { myTheme } from './theme'
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET as string
+
+export default defineConfig({
+  basePath: "/studio",
+  name: 'PAPAFMA_Content_Studio',
+  title: 'PAPAFAM Content studio',
+  projectId,
+  dataset,
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+  theme: myTheme
+})
